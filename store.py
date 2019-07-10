da079429e31d8c572d7f68dab489f115bc842b47
Carla Miles <ms.carla.albert@gmail.com>
2019-07-10 12:05:07 -0500

    updated some methods after class demo

--- store.py
+++ store.py
@@ -7,11 +7,14 @@ class Product:
     def update_price(self, percent_change, is_increased):
         if is_increased == True:
             self.price = self.price + (self.price * percent_change)
-        elif is_increased == False:
+        else:
             self.price = self.price - (self.price * percent_change)
+        return self
     
     def print_info(self):
         print("Product name: ", self.name, "Category: ", self.category, "Price: ", self.price)
+        return self
+
 
 class Store:
     def __init__(self, store_name):
@@ -20,19 +23,40 @@ class Store:
                     
     def add_product(self, new_product):
         self.product_list.append(new_product)
+        return self
     
     def sell_product(self, id):
         print(self.product_list[id])
         del self.product_list[id]
+        return self
+        # or could write it like this:
+        # sold_product = self.products.pop(id)
+        # sold_product.print_info()
+        # return self
     
     def inflation(self, percent_increase):
         for i in range(len(self.product_list)):
             self.product_list[i].update_price(percent_increase, True)
+            return self
+        # or could write it like this:
+        # for product in self.product_list:
+        #   product.update_price(percent_increase, True)
+        # return self
     
     def set_clearance(self, category, percent_discount):
         for i in range(len(self.product_list)):
             if self.product_list[i].category == category:
                 self.product_list[i].update_price(percent_discount, False)
+                return self
+        # or could write it like this:
+        # for product in self.product_list:
+        #   product.update_price(percent_discount, False)
+        # return self
+
+    def show_products(self):
+        for product in self.product_list:
+            product.print_info()
+        return self
 
 
 aldi = Store("aldi")
@@ -42,10 +66,7 @@ mild_salsa = Product("mild_salsa", 3, "snacks")
 frozen_chicken = Product("frozen_chicken", 6, "frozen")
 glazed_donuts = Product("glazed_donuts", 4, "bakedGoods")
 
-aldi.add_product(sour_cream_chips)
-aldi.add_product(mild_salsa)
-aldi.add_product(frozen_chicken)
-aldi.add_product(glazed_donuts)
+aldi.add_product(sour_cream_chips).add_product(mild_salsa).add_product(frozen_chicken).add_product(glazed_donuts)
 
 print(aldi.product_list)
 
@@ -63,3 +84,4 @@ frozen_chicken.print_info()
 
 aldi.sell_product(0)
 
+aldi.show_products()
